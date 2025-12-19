6428c5c25bbc698b67c66508f9545d7c2ae3c11a
Angelhpascual <Angelhpascual@gmail.com>
2025-12-19 11:51:22 +0100

    feat: Initialize project with Tailwind CSS, path aliases, and core UI/3D dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@domain": path.resolve(__dirname, "./src/domain"),
+      "@application": path.resolve(__dirname, "./src/application"),
+      "@infrastructure": path.resolve(__dirname, "./src/infrastructure"),
+      "@ui": path.resolve(__dirname, "./src/ui"),
+    },
+  },
 })
